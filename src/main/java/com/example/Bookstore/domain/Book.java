d5e1c86605337fd1af2d88285e1c94802bb1900c
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-12 20:48:01 +0300

    Create Book entity

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,17 +1,36 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String author;
     private int isbn;
     private double price;
+
+    public Book() {}
 
     public Book(String title, String author, int isbn, double price) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -48,6 +67,6 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book [title='" + title + "', author='" + author + "', isbn=" + isbn + ", price=" + price + "]";
+        return "Book [id= " + id + ", title='" + title + "', author='" + author + "', isbn=" + isbn + ", price=" + price + "]";
     }
 }
