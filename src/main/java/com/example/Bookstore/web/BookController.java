e20ced63c8a7bec63beb5dc81c1c58be6c660f6d
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-15 12:46:31 +0300

    Add book list view and template

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,14 +1,22 @@
 package com.example.Bookstore.web;
 
+import com.example.Bookstore.domain.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class BookController {
 
-    @GetMapping("index")
-    public String index() {
-        return "index";
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping("/booklist")
+    public String bookstore(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 
 }
