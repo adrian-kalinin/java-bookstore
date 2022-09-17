384794324dacceeb4e5b75e01079b786b7350b31
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-17 16:27:31 +0300

    Refactor

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -24,8 +24,8 @@ public class BookstoreApplication {
 		return (args) -> {
 			log.info("Creating and saving a few demo books.");
 
-			Book firstBook = new Book("12 Rules for Life", "Jordan Peterson", 2018, "9783442315536", 19.99);
-			Book secondBook = new Book("Atlas Shrugged", "Ayn Rand", 1957, "9780451132154", 9.99);
+			Book firstBook = new Book("12 Rules for Life", "Jordan Peterson", 2018, "978-34-42315-53-6", 19.99);
+			Book secondBook = new Book("Atlas Shrugged", "Ayn Rand", 1957, "978-04-51132-15-4", 9.99);
 
 			repository.save(firstBook);
 			repository.save(secondBook);
