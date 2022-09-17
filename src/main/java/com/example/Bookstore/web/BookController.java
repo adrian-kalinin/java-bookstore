2eb576e409ee86e177af70bcfd15c9fe5d0a6077
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-17 13:32:07 +0300

    Add delete book functionality

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,8 +4,10 @@ import com.example.Bookstore.domain.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BookController {
@@ -22,6 +24,14 @@ public class BookController {
     public String bookstore(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        if (repository.existsById(bookId)) {
+            repository.deleteById(bookId);
+        }
+        return "redirect:../booklist";
     }
 
 }
