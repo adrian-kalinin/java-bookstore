334b19ce29863601f9260d524d468234f72eff1e
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-17 16:25:59 +0300

    Add create book functionality

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,12 +1,18 @@
 package com.example.Bookstore.web;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import javax.validation.Valid;
 
 @Controller
 public class BookController {
@@ -22,7 +28,23 @@ public class BookController {
     @GetMapping("/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
-        return "booklist";
+        return "bookList";
+    }
+
+    @GetMapping("/book/create")
+    public String createBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addBook";
+    }
+
+    @PostMapping("/book/create")
+    public String createBook(@Valid @ModelAttribute("book") Book newBook, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "addBook";
+        }
+
+        repository.save(newBook);
+        return "redirect:../booklist";
     }
 
     @GetMapping("/book/{id}/delete")
