11b1d00daa7332f3483622c9dbb6bab03ded4072
Adrian Kalinin <adriankalinin@protonmail.com>
2022-09-20 22:13:12 +0300

    Add demo data for categories

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -2,6 +2,8 @@ package com.example.Bookstore;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,23 +15,29 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
-	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
-		return (args) -> {
-			log.info("Creating and saving a few demo books.");
+    @Bean
+    public CommandLineRunner bookDemo(CategoryRepository categoryRepository, BookRepository bookRepository) {
+        return (args) -> {
+            log.info("Creating and saving a few demo books.");
 
-			Book firstBook = new Book("12 Rules for Life", "Jordan Peterson", 2018, "978-34-42315-53-6", 19.99);
-			Book secondBook = new Book("Atlas Shrugged", "Ayn Rand", 1957, "978-04-51132-15-4", 9.99);
+            Category firstCategory = new Category("Self-help");
+            Category secondCategory = new Category("Novel");
 
-			repository.save(firstBook);
-			repository.save(secondBook);
-		};
-	}
+            categoryRepository.save(firstCategory);
+            categoryRepository.save(secondCategory);
+
+            Book firstBook = new Book("12 Rules for Life", "Jordan Peterson", 2018, "978-34-42315-53-6", 19.99, firstCategory);
+            Book secondBook = new Book("Atlas Shrugged", "Ayn Rand", 1957, "978-04-51132-15-4", 9.99, secondCategory);
+
+            bookRepository.save(firstBook);
+            bookRepository.save(secondBook);
+        };
+    }
 
 }
