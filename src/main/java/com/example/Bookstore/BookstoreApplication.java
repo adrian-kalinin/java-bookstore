b8467207ea9e0083ebf2fec1f47efe62bea04771
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-06 12:49:45 +0200

    Change List.of() to Arrays.asList() for Java 8

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -14,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
-import java.util.List;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -31,7 +31,7 @@ public class BookstoreApplication {
             log.info("Creating and saving demo data.");
 
             // Create categories
-            categoryRepository.saveAll(List.of(
+            categoryRepository.saveAll(Arrays.asList(
                     new Category("Self-help"),
                     new Category("Novel"),
                     new Category("Fantasy"),
@@ -40,13 +40,13 @@ public class BookstoreApplication {
             ));
 
             // Create books
-            bookRepository.saveAll(List.of(
+            bookRepository.saveAll(Arrays.asList(
                     new Book("12 Rules for Life", "Jordan Peterson", 2018, "978-34-42315-53-6", 19.99, categoryRepository.findById(1L).orElse(null)),
                     new Book("Atlas Shrugged", "Ayn Rand", 1957, "978-04-51132-15-4", 9.99, categoryRepository.findById(2L).orElse(null))
             ));
 
             // Create users
-            userRepository.saveAll(List.of(
+            userRepository.saveAll(Arrays.asList(
                     new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@example.com", "USER"),
                     new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@example.com", "ADMIN")
             ));
