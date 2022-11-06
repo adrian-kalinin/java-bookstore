5aa52c6e12ca8beb9f6661ea9b87dc84663f2abe
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-06 11:15:53 +0200

    Add findAllByName to CategoryRepository

--- src/main/java/com/example/Bookstore/repository/CategoryRepository.java
+++ src/main/java/com/example/Bookstore/repository/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.example.Bookstore.repository;
 
 import com.example.Bookstore.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByName(@Param("name") String name);
 }
