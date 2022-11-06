4a6454a84c2e6739f41734ce5afdc80271dfae9f
Adrian Kalinin <adriankalinin@protonmail.com>
2022-11-06 11:07:21 +0200

    Use JpaRepository instead of CrudRepository

--- src/main/java/com/example/Bookstore/repository/BookRepository.java
+++ src/main/java/com/example/Bookstore/repository/BookRepository.java
@@ -1,14 +1,14 @@
 package com.example.Bookstore.repository;
 
 import com.example.Bookstore.model.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(@Param("title") String title);
 
